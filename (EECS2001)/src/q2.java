ddea0f733d1755e063d97364f74c2d70b8fa4900
jason <jason@LAPTOP-L90IPECE>
2017-11-09 15:19:08 -0500

    q2

---
+++ (EECS2001)/src/q2.java
@@ -0,0 +1,33 @@
+
+public class q2 {
+
+	public static boolean parseLoop(String str) {
+		String s = str;
+		while (true) {
+
+			if (str.equals("A")) {
+				System.out.println("TRUE: "+s + " is a valid string.");
+				return true;
+			} else if (str.equals("Ab")) {
+				System.out.println("TRUE: "+s + " is a valid string.");
+				return true;
+			} else if (str.length() < 2) {
+				System.out.println("FALSE: "+s + " is NOT a valid String.");
+				return false;
+			} else if (str.substring(0, 2).equals("Aa")) {
+				str = str.substring(2);
+			} else {
+				System.out.println("FALSE: "+s + " is NOT a valid String.");
+				return false;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		q2.parseLoop("AaAaAaAaAaAaAaAAaAaAaAaAaAaAaAaAb");
+		q2.parseLoop("AaAaAaAA");
+		q2.parseLoop("AaAaAaA");
+		q2.parseLoop("bb");
+		q2.parseLoop("");
+	}
+}
